728f9aecef3b672d2c75b729a740f91c0407f483
scu-ljy <532270384@qq.com>
2023-03-21 22:29:16 +0800

    Update vina_docking.sh

--- vina/vina_docking.sh
+++ vina/vina_docking.sh
@@ -7,30 +7,39 @@ source ~/miniconda3/etc/profile.d/conda.sh
 
 echo "开始批处理docking:"
 
+
+echo "请输入本次docking的根目录, 以绝对路径给出 (例: /home/chaoyideng/Dy/ADT/m55/"
+read DIR
 echo "请输入包含多个分子的文件名(不包含.mol2, 例: 20210521_Mol55):"
-read inpf
+read INPF
 echo "请输入保存每个分子的文件名前缀(不包含.mol2, 例: m55):"
-read outf
+read OUTF
 echo "请输入文件中含有的分子个数:"
 read N
 echo "请输入配置文件的绝对路径（例：/home/chaoyideng/Dy/ADT/config_file.arg):"
-read config_file
+read CONFIG_FILE
 echo "请输入输出日志文件的名字（不包含.log, 例: docking_log):"
-read log_file
+read LOG_FILE
 echo "请输入转存日志文件的文件夹(例: logs):"
-read log_folder
+read LOG_FOLDER
 echo "请输入亲和度数据文件名(例: affinity.out)"
-read affinity_file
+read AFFINITY_FILE
 echo "请输入分子compound前缀名(例: AL, m3000, m2000等)"
-read prefix
+read PREFIX
 
 num=$(expr $N + 0)
+
+CURRENT_DIR=$(pwd)
+echo "当前目录为：$CURRENT_DIR"
+
+echo "切换至:${DIR}目录"
+cd $DIR
 
 echo "1. 用obabel将含有多个分子的mol2文件拆分成多个mol2文件:"
 # 用obabel将含有多个分子的mol2文件拆分成多个mol2文件
 conda activate vina
-# /home/chaoyideng/miniconda3/envs/vina/bin/obabel -i mol2 ${inpf}.mol2 -o mol2 -O ${outf}_.mol2 -m
-obabel -i mol2 ${inpf}.mol2 -o mol2 -O ${outf}_.mol2 -m
+# /home/chaoyideng/miniconda3/envs/vina/bin/obabel -i mol2 ${INPF}.mol2 -o mol2 -O ${OUTF}_.mol2 -m
+obabel -i mol2 ${INPF}.mol2 -o mol2 -O ${OUTF}_.mol2 -m
 conda deactivate
 
 
@@ -41,7 +50,7 @@ mkdir $(eval echo {1..${num}})
 
 # 将配体放入个字文件夹内，并重命名
 for i in $(eval echo {1..${num}});do
-  mv ./${outf}_${i}.mol2 ./$i/${outf}.mol2;
+  mv ./${OUTF}_${i}.mol2 ./$i/${OUTF}.mol2;
 done
 
 echo "3. 批量将mol2文件转换为pdbqt"
@@ -49,29 +58,32 @@ echo "3. 批量将mol2文件转换为pdbqt"
 conda activate mgltools
 for i in $(eval echo {1..${num}});do
   cd ./$i;
-  prepare_ligand4.py -l ${outf}.mol2 -o ${outf}.pdbqt;
+  prepare_ligand4.py -l ${OUTF}.mol2 -o ${OUTF}.pdbqt;
   echo "转换文件${i}完成!";
   cd ../;
 done
 conda deactivate
 
-echo "4. 使用vina批量对接，并将结果输出到每个文件夹中的log文件"
+echo "4. 使用vina批量对接, 并将结果输出到每个文件夹中的log文件"
 conda activate vina
 for i in $(eval echo {1..${num}});do
   cd ./$i;
-  vina --config $config_file > ./$log_file.log;
+  vina --config $CONFIG_FILE > ./$LOG_FILE.log;
   echo "对接完第${i}个分子!";
   cd ../;
 done
 conda deactivate
 
 echo "5. 对接完成, 提取log日志文件"
-mkdir $log_folder
+mkdir $LOG_FOLDER
 for i in $(eval echo {1..${num}});do
   cd ./$i;
-  mv $log_file.log ../$log_folder/${log_file}_${i}.log
+  mv $LOG_FILE.log ../$LOG_FOLDER/${LOG_FILE}_${i}.log
   cd ../;
 done
 
-echo "6. 提取亲和度数据至输出文件$affinity_file"
-python affinity_extract.py "$log_folder" "$prefix"
+echo "6. 提取亲和度数据至输出文件$AFFINITY_FILE"
+python $CURRENT_DIR/affinity_extract.py "$LOG_FOLDER" "$PREFIX"
+
+#切换回之前的目录
+cd $CURRENT_DIR
